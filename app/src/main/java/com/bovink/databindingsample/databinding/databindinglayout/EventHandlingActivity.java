f929132076482a49c4557e1833a4f818e038111a
bovink <bovink@sina.com>
2018-03-17 17:36:05 +0800

    add event handling sample

--- app/src/main/java/com/bovink/databindingsample/databinding/databindinglayout/EventHandlingActivity.java
+++ app/src/main/java/com/bovink/databindingsample/databinding/databindinglayout/EventHandlingActivity.java
@@ -3,8 +3,10 @@ package com.bovink.databindingsample.databinding.databindinglayout;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.bovink.databindingsample.databinding.EventHandlerActBinding;
+import com.bovink.databindingsample.model.PojoUser;
 
 
 /**
@@ -21,5 +23,21 @@ public class EventHandlingActivity extends AppCompatActivity {
         EventHandlerActBinding binding = EventHandlerActBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        PojoUser bruce = new PojoUser("Bruce", "Wayne");
+        binding.setBruce(bruce);
+
+        PojoUser bane = new PojoUser("Bane", "");
+        binding.setBane(bane);
+
+        Choice choice = new Choice();
+        binding.setChoice(choice);
+    }
+
+    public class Choice {
+
+        public void kickAss(PojoUser whosleg, PojoUser whosass) {
+
+            Toast.makeText(EventHandlingActivity.this, whosleg.lastName + " is kicking " + whosass.lastName + "'s ass.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
