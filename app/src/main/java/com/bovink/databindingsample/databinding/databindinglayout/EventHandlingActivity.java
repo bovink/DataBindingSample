ccc40fdff6f4ea2553188e1c19a7f818a58afd52
bovink <bovink@sina.com>
2018-03-17 21:15:40 +0800

    add event handling sample

--- app/src/main/java/com/bovink/databindingsample/databinding/databindinglayout/EventHandlingActivity.java
+++ app/src/main/java/com/bovink/databindingsample/databinding/databindinglayout/EventHandlingActivity.java
@@ -1,8 +1,10 @@
 package com.bovink.databindingsample.databinding.databindinglayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.bovink.databindingsample.databinding.EventHandlerActBinding;
@@ -26,7 +28,10 @@ public class EventHandlingActivity extends AppCompatActivity {
         PojoUser bruce = new PojoUser("Bruce", "Wayne");
         binding.setBruce(bruce);
 
-        PojoUser bane = new PojoUser("Bane", "");
+        PojoUser clark = new PojoUser("Clark", "Kent");
+        binding.setClark(clark);
+
+        PojoUser bane = new PojoUser("", "Bane");
         binding.setBane(bane);
 
         Choice choice = new Choice();
@@ -37,7 +42,13 @@ public class EventHandlingActivity extends AppCompatActivity {
 
         public void kickAss(PojoUser whosleg, PojoUser whosass) {
 
-            Toast.makeText(EventHandlingActivity.this, whosleg.lastName + " is kicking " + whosass.lastName + "'s ass.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(EventHandlingActivity.this, whosleg.lastName + " is kicking " + whosass.lastName + "'s ass", Toast.LENGTH_SHORT).show();
+        }
+
+        public void runAway(View view, PojoUser whoIsRun, PojoUser chaserA, PojoUser chaserB) {
+
+            view.setBackgroundColor(Color.parseColor("#66CCFF"));
+            Toast.makeText(EventHandlingActivity.this, whoIsRun.lastName + " is run away from " + chaserA.lastName + " and " + chaserB.lastName, Toast.LENGTH_SHORT).show();
         }
     }
 }
