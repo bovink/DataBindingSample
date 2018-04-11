42aae1d3da9513e3b543cd4b4e6154bfb218a23c
bovink@sina.com <261512335wen>
2018-04-11 17:03:35 +0800

    add include example

--- app/src/main/java/com/bovink/databindingsample/databinding/layoutdetails/IncludeActivity.java
+++ app/src/main/java/com/bovink/databindingsample/databinding/layoutdetails/IncludeActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bovink.databindingsample.databinding.IncludeActBinding;
-import com.bovink.databindingsample.model.Person;
+import com.bovink.databindingsample.model.User;
 
 
 /**
@@ -22,10 +22,7 @@ public class IncludeActivity extends AppCompatActivity {
         IncludeActBinding binding = IncludeActBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Person person = new Person();
-        person.setName("super man");
-        person.setSex("male");
-
-        binding.setPerson(person);
+        User user = new User("Bruce", "Wayne", true);
+        binding.setUser(user);
     }
 }
